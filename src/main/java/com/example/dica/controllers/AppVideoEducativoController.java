fc9356e58a1de8e6390c19b07e1bf9afc1dc0521
Pedro Vergeth <pedrovergeth62@hotmail.com>
2026-04-27 16:40:29 -0300

    Criando controller app videos educativos

---
+++ src/main/java/com/example/dica/controllers/AppVideoEducativoController.java
@@ -0,0 +1,38 @@
+package com.example.dica.controllers;
+
+import com.example.dica.domain.videoEducativo.VideoEducativoResponseDto;
+import com.example.dica.domain.videoEducativo.VideoEducativoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/app/video-educativo")
+public class AppVideoEducativoController {
+
+    @Autowired
+    private VideoEducativoService videoEducativoService;
+
+    @GetMapping
+    public ResponseEntity<Page<VideoEducativoResponseDto>> listarVideosEducativos(
+            @RequestParam(defaultValue = "") String buscaLivre,
+            @PageableDefault(sort = "id") Pageable pageable) {
+
+        var videos = videoEducativoService.getAll(pageable, buscaLivre);
+        var response = videos.map(VideoEducativoResponseDto::new);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VideoEducativoResponseDto> getVideoEducativoById(@PathVariable Long id) {
+        var videoEducativo = videoEducativoService.getById(id);
+        return ResponseEntity.ok(new VideoEducativoResponseDto(videoEducativo));
+    }
+}
